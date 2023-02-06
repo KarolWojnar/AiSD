425b444e447c7765474588ec1dc7522c95465bc9
KarolWojnar <103566194+KarolWojnar@users.noreply.github.com>
2023-02-06 09:51:43 +0100

    Update DynArray.java

--- A/AZ1/DynArray.java
+++ A/AZ1/DynArray.java
@@ -1,92 +1,89 @@
-package AZ1;
-
-public class DynArray {
-    private Person[] table;
-    private int nElems;
-    DynArray(int maxSize)
+public class ArrayNotSort {
+    public Person[] tab;
+    public int elem;
+    ArrayNotSort(int size)
     {
-        this.table = new Person[maxSize];
-        this.nElems = 0;
-    }
-    public int size()
-    {
-        return nElems;
+        tab = new Person[size];
+        elem = 0;
     }
     public void add(Person p)
     {
-        if(nElems >= table.length)
+        if(elem >= tab.length)
         {
-            Person[] tempTab = new Person[table.length * 2];
-            for(int i = 0; i < table.length; i++) tempTab[i] = table[i];
-            table = tempTab;
+            Person[] temp = new Person[tab.length * 2];
+            for(int i = 0; i < elem; i++) temp[i] = tab[i];
+            tab = temp;
         }
-        table[nElems] = p;
-        nElems++;
+        tab[elem] = p;
+        elem++;
     }
     public Person get(int index)
     {
-        if(index >= nElems || index < 0) return null;
-        return table[index];
+        if(index >= elem || index < 0) return null;
+        return tab[index];
     }
-    public Person find(Person p)
+    public int size()
     {
-        for(int i = 0; i < nElems; i++)
-        {
-            if(table[i].getImie().equals(p.getImie())
-            && table[i].getNazwisko().equals(p.getNazwisko())
-            && table[i].getWiek() == p.getWiek()) return table[i];
-        }
-        return null;
+        return elem;
     }
-    public boolean remove(Person p)
+    public boolean remove(int index)
     {
-        int i;
-        for(i = 0; i < nElems; i++)
+        if(index >= elem || index < 0) return false;
+        for(int i = index; i < elem - 1; i++)
         {
-            if(table[i].getImie().equals(p.getImie())
-                    && table[i].getNazwisko().equals(p.getNazwisko())
-                    && table[i].getWiek() == p.getWiek()) break;
+            tab[i] = tab[i + 1];
         }
-        if(i == (nElems - 1)){
-            table[nElems] = null;
-            nElems--;
-            return true;
-        }
-        else if(i < (nElems - 1)) {
-            for (int j = i; j < (nElems - 1); j++) {
-                table[j] = table[j + 1];
-            }
-            nElems--;
-            return true;
-        }
-        return false;
+        elem--;
+        return true;
     }
-    public void print() {
-        for (int i = 0; i < nElems; i++)
+    public int find(Person p)
+    {
+        for(int i = 0 ; i < elem; i++)
         {
-            System.out.println(table[i]);
+            if(tab[i].compareTo(p) == 0) return i;
+        }
+        return -1;
+    }
+    public int find2(Person p)
+    {
+        for(int i = 0; i < elem; i++)
+        {
+            if(tab[i].getImie().equals(p.getImie())
+            && tab[i].getNazwisko().equals(p.getNazwisko())
+            && tab[i].getWiek() == p.getWiek()) return i;
+        }
+        return -1;
+    }
+    public void print()
+    {
+        for(int i = 0; i < elem; i++)
+        {
+            System.out.println(tab[i]);
+        }
+    }
+    public void print2()
+    {
+        for(int i = 0; i < elem; i++)
+        {
+            System.out.println("Imie:\t" + tab[i].getImie() + "\tnazwisko:\t" + tab[i].getNazwisko() + "\twiek:\t" + tab[i].getWiek());
         }
     }
 
     public static void main(String[] args) {
-        DynArray tablica = new DynArray(1);
-        tablica.add(new Person("Janek", "Jankowski", 44));
-        tablica.add(new Person("Kamil", "Grabowski", 33));
-        tablica.add(new Person("Wojtek", "Frankowski", 22));
-        tablica.add(new Person("Michal", "Dabrowski", 11));
-        tablica.add(new Person("Adam", "Cwierkowski", 55));
-        tablica.add(new Person("Bartek", "Nowak", 66));
-        tablica.add(new Person("Damian", "Kowalski", 77));
-        tablica.print();
-        System.out.println(tablica.find(new Person("Janek", "Jankowski", 41)));
-        System.out.println(tablica.get(5));
-        System.out.println(tablica.size());
-        tablica.remove(new Person("Wojtek", "Frankowski", 22));
-        tablica.print();
-        System.out.println(tablica.size());
+        ArrayNotSort nowa = new ArrayNotSort(1);
+        nowa.add(new Person("Janek", "Nowak", 44));
+        nowa.add(new Person("Adam", "Wojt", 22));
+        nowa.add(new Person("Basia", "Kiszka", 11));
+        nowa.add(new Person("Celina", "Baryła", 33));
+        nowa.add(new Person("Daria", "Ludwik", 66));
+        nowa.add(new Person("Ewa", "Kowal", 51));
+        nowa.print();
+        nowa.remove(4);
+        nowa.print2();
     }
 }
-class Person implements Comparable<Person>{
+class Person implements Comparable<Person>
+{
     private String imie, nazwisko;
     private int wiek;
 
@@ -96,28 +93,26 @@ class Person implements Comparable<Person>{
         this.wiek = wiek;
     }
 
-    public int getWiek() {
-        return wiek;
-    }
-
-    public void setWiek(int wiek) {
-        this.wiek = wiek;
-    }
-
     public String getImie() {
         return imie;
-    }
-
-    public void setImie(String imie) {
-        this.imie = imie;
     }
 
     public String getNazwisko() {
         return nazwisko;
     }
 
-    public void setNazwisko(String nazwisko) {
-        this.nazwisko = nazwisko;
+    public int getWiek() {
+        return wiek;
+    }
+
+    @Override
+    public int compareTo(Person o) {
+        if(this.getWiek() > o.getWiek()) return 1;
+        else if(this.getWiek() < o.getWiek()) return -1;
+        else{
+            if(this.getImie().compareTo(o.getImie()) != 0) return this.getImie().compareTo(o.getImie());
+            else return this.getNazwisko().compareTo(o.getNazwisko());
+        }
     }
 
     @Override
@@ -127,17 +122,5 @@ class Person implements Comparable<Person>{
                 ", nazwisko='" + nazwisko + '\'' +
                 ", wiek=" + wiek +
                 '}';
-    }
-
-    @Override
-    public int compareTo(Person o) {
-        //wiek
-        //imie
-        //nazwisko
-        if(this.getWiek() != o.getWiek()) return 1;
-        else{
-            if(!this.getImie().equals(o.getImie())) return this.getImie().compareTo(o.getImie());
-            else return this.getNazwisko().compareTo(o.getNazwisko());
-        }
     }
 }
