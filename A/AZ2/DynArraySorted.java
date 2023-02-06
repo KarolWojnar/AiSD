c1c952c3afa2eb8f24c6d964a3f5e518f3785e93
KarolWojnar <103566194+KarolWojnar@users.noreply.github.com>
2023-02-06 09:52:23 +0100

    Update DynArraySorted.java

--- A/AZ2/DynArraySorted.java
+++ A/AZ2/DynArraySorted.java
@@ -1,96 +1,83 @@
-package AZ2;
-
-import AZ1.DynArray;
-
-public class DynArraySorted {
-    private Person[] table;
-    private int nElems;
-    DynArraySorted(int maxSize)
+public class SortowanaArray {
+    public Person[] tab;
+    public int size;
+    SortowanaArray(int size)
     {
-        this.table = new Person[maxSize];
-        this.nElems = 0;
-    }
-    public int size()
-    {
-        return nElems;
+        tab = new Person[size];
+        this.size = 0;
     }
     public void add(Person p)
     {
-        if (nElems >= table.length) {
-            Person[] tempTab = new Person[table.length * 2];
-            for (int i = 0; i < table.length; i++) tempTab[i] = table[i];
-            table = tempTab;
+        if(size >= tab.length)
+        {
+            Person[] temp = new Person[tab.length * 2];
+            for (int i = 0; i < size; i++) temp[i] = tab[i];
+            tab = temp;
         }
         int i;
-        for (i = 0; i < nElems; i++) {
-            if (table[i].compareTo(p) > 0) break;
+        for(i = 0; i < size; i++)
+        {
+            if(tab[i].compareTo(p) > 0) break;
         }
-        for (int j = nElems; j > i; j--) {
-            table[j] = table[j - 1];
+        for(int j = size; j > i; j--)
+        {
+            tab[j] = tab[j - 1];
         }
-        table[i] = p;
-        nElems++;
+        tab[i] = p;
+        size++;
     }
     public Person get(int index)
     {
-        if(index >= nElems || index < 0) return null;
-        return table[index];
+        if(index >= size || index < 0) return null;
+        return tab[index];
     }
-    public Person find(Person p)
+    public int size(){return size;}
+    public boolean remove(int index)
     {
-        for(int i = 0; i < nElems; i++)
+        if(index >= size || index < 0) return false;
+        for(int i = index; i < size - 1; i++)
         {
-            if(table[i].compareTo(p) == 0) return table[i];
+            tab[i] = tab[i + 1];
         }
-        return null;
+        size--;
+        return true;
     }
-    public boolean remove(Person p)
+    public int find(Person p)
     {
-        int i;
-        for(i = 0; i < nElems; i++)
+        for(int i = 0; i< size; i++)
         {
-            if(table[i].compareTo(p) == 0) break;
+            if(tab[i].getImie().equals(p.getImie())
+            && tab[i].getNazwisko().equals(p.getNazwisko())
+            && tab[i].getWiek() == p.getWiek()) return i;
         }
-        if(i == (nElems - 1)){
-            table[nElems] = null;
-            nElems--;
-            return true;
-        }
-        else if(i < (nElems - 1)) {
-            for (int j = i; j < (nElems - 1); j++) {
-                table[j] = table[j + 1];
-            }
-            nElems--;
-            return true;
-        }
-        return false;
+        return -1;
     }
-    public void print() {
-        for (int i = 0; i < nElems; i++)
+    public int find2(Person p)
+    {
+        for(int i = 0; i < size; i++) if(tab[i].compareTo(p) == 0 )return i;
+        return -1;
+    }
+    public void print()
+    {
+        for(int i = 0; i < size; i++)
         {
-            System.out.println(table[i]);
+            System.out.println(tab[i]);
         }
     }
 
     public static void main(String[] args) {
-        DynArraySorted tablica = new DynArraySorted(1);
-        tablica.add(new Person("Janek", "Jankowski", 44));
-        tablica.add(new Person("Kamil", "Grabowski", 33));
-        tablica.add(new Person("Wojtek", "Frankowski", 22));
-        tablica.add(new Person("Michal", "Dabrowski", 11));
-        tablica.add(new Person("Adam", "Cwierkowski", 22));
-        tablica.add(new Person("Bartek", "Nowak", 66));
-        tablica.add(new Person("Damian", "Kowalski", 77));
-        tablica.print();
-//        System.out.println(tablica.find(new Person("Janek", "Jankowski", 41)));
-//        System.out.println(tablica.get(5));
-//        System.out.println(tablica.size());
-//        tablica.remove(new Person("Wojtek", "Frankowski", 22));
-//        tablica.print();
-//        System.out.println(tablica.size());
+        SortowanaArray sortowana = new SortowanaArray(1);
+        sortowana.add(new Person("Kamil", "Nowak", 33));
+        sortowana.add(new Person("Krystian", "Kuras", 22));
+        sortowana.add(new Person("Mariusz", "Berkowicz", 31));
+        sortowana.add(new Person("Wiktoria", "Wojtowicz", 11));
+        sortowana.add(new Person("Julia", "Lisek", 23));
+        sortowana.add(new Person("Kamila", "Kowal", 33));
+        sortowana.print();
     }
 }
-class Person implements Comparable<Person>{
+class Person implements Comparable<Person>
+{
     private String imie, nazwisko;
     private int wiek;
 
@@ -104,26 +91,6 @@ class Person implements Comparable<Person>{
         return wiek;
     }
 
-    public void setWiek(int wiek) {
-        this.wiek = wiek;
-    }
-
-    public String getImie() {
-        return imie;
-    }
-
-    public void setImie(String imie) {
-        this.imie = imie;
-    }
-
-    public String getNazwisko() {
-        return nazwisko;
-    }
-
-    public void setNazwisko(String nazwisko) {
-        this.nazwisko = nazwisko;
-    }
-
     @Override
     public String toString() {
         return "Person{" +
@@ -133,17 +100,21 @@ class Person implements Comparable<Person>{
                 '}';
     }
 
+    public String getNazwisko() {
+        return nazwisko;
+    }
+
+    public String getImie() {
+        return imie;
+    }
+
     @Override
     public int compareTo(Person o) {
-        //wiek
-        //imie
-        //nazwisko
         if(this.getWiek() > o.getWiek()) return 1;
-        if(this.getWiek() < o.getWiek()) return -1;
-        else if(this.getWiek() == o.getWiek()){
+        else if(this.getWiek() < o.getWiek()) return -1;
+        else {
             if(this.getImie().compareTo(o.getImie()) != 0) return this.getImie().compareTo(o.getImie());
             else return this.getNazwisko().compareTo(o.getNazwisko());
         }
-        return 0;
     }
 }
